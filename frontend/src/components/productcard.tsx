c9ecb1747aae86bea6a4de0a645fac5c97d6003c
pizza-eater-3500 <pizzaeater3500@gmail.com>
2025-08-04 21:41:08 -0400

    implement product card ui

---
+++ frontend/src/components/productcard.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import type Product from "../../types/ProductType.ts";
+import { EditIcon, Trash2Icon } from "lucide-react";
+import useProducts from "../hooks/useProducts.ts";
+
+interface IProps {
+    product: Product;
+}
+
+export default function ProductCard({ product }: IProps) {
+    const { deleteProduct } = useProducts();
+
+    return (
+        <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
+            {/* Product Image */}
+            <figure className="relative pt-[56.25%]">
+                <img src={product.image} alt={product.name} className="absolute top-0 left-0 w-full h-full object-cover" />
+            </figure>
+
+            {/* Product Description */}
+            <div className="card-body">
+                <h2 className="card-title text-lg font-semibold">{product.name}</h2>
+                <p className="text-2xl font-bold text-primary">${Number(product.price).toFixed(2)}</p>
+
+                {/* Product Actions */}
+                <div className="card-actions justify-end mt-4">
+                    <Link to={`/product/${product.id}`} className="btn btn-sm btn-info btn-outline">
+                        <EditIcon className="size-4" />
+                    </Link>
+
+                    <button className="btn btn-sm btn-error btn-outline" onClick={()=>deleteProduct(product.id)}>
+                        <Trash2Icon className="size-4" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
